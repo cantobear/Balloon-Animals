af74ddc6a6cbb00354467defb997e4f3b9d4a84f
matthekg <matthekg@uci.edu>
2016-10-24 15:51:18 -0700

    Player movement bug fixes
    -Fixed player being able to jump endlessly if touching a wall
    -Fixed player being able to stick to a wall if holding towards it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Collider2D))]
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private Collider2D coll;
 	private bool grounded;
+
+	public float maxHorizontalSpeed = 5f;
+	public float horizontalAccel = 50f;
+	public float jumpAccel = 500f;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		coll = GetComponent<Collider2D>();
 	}
-	
+
 	// Update is called once per frame
 	void Update(){
-		if (!grounded && rb.velocity.y == 0) {
+
+
+
+		if (coll.IsTouchingLayers(LayerMask.GetMask ("Ground"))) {
 			grounded = true;
 		}
 	}
 	void FixedUpdate () {
 	
 		if ((Input.GetKey ("up") || Input.GetKey (KeyCode.W)) && grounded) {
-			rb.AddForce (transform.up * 100);
+			rb.AddForce (transform.up * jumpAccel);
 			grounded = false;
 		}
 		if (Input.GetKey("right") || Input.GetKey (KeyCode.D)) {
-			rb.AddForce (transform.right * 10);
+			rb.AddForce (transform.right * horizontalAccel);
 		}
 		if (Input.GetKey ("left") || Input.GetKey (KeyCode.A)) {
-			rb.AddForce (transform.right * -10);
+			rb.AddForce (transform.right * -horizontalAccel);
 		}
+
+		//Caps the player's horizontal velocity
+		Vector2 velocity = rb.velocity;
+		velocity.x = Mathf.Clamp (velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed);
+		rb.velocity = velocity;
 	}
 }
