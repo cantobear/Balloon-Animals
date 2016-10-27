c1de28a57a1a360be1f6b56b5178322844654f26
notdoods <khchao@uci.edu>
2016-10-27 16:22:49 -0700

    Counter + Click Spawner
    Added counter when balloon (Circle for now) hits floor and click spawns
    balloons

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour
+{
+
+   // Ray ray;
+    //RaycastHit hit;
+    public GameObject balloon;
+    public Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            {
+            var mousePos = Input.mousePosition;
+            mousePos.z = 2f;
+            var position = cam.ScreenToWorldPoint(mousePos);
+            position.z = 0;
+            Instantiate(balloon, position, Quaternion.identity);
+            }
+    }
+}
---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+    private int count;
+    public GUIText scoreText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        count = 0;
+        UpdateCount();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Balloon"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            UpdateCount();
+        }
+    }
+    void UpdateCount()
+    {
+        scoreText.text = "Counter: " + count;
+    }
+}
