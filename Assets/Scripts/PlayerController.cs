a41c5a41b6580d47d7e4e26317bff4bfaf7abe36
matthekg <matthekg@uci.edu>
2016-10-27 16:54:43 -0700

    player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,24 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	private bool grounded;
+	private Collider2D coll;
 
 	public float maxHorizontalSpeed = 5f;
 	public float horizontalAccel = 50f;
+	public float jumpAccel = 500f;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		coll = GetComponent<Collider2D>();
 	}
 
 	// Update is called once per frame
 	void Update(){
+		if (coll.IsTouchingLayers(LayerMask.GetMask ("Ground"))) {
+			grounded = true;
+		}
 	}
 	void FixedUpdate () {
 
@@ -24,6 +31,10 @@ public class PlayerController : MonoBehaviour {
 		}
 		if (Input.GetKey ("left") || Input.GetKey (KeyCode.A)) {
 			rb.AddForce (transform.right * -horizontalAccel);
+		}
+		if ((Input.GetKey ("up") || Input.GetKey (KeyCode.W)) && grounded) {
+			rb.AddForce (transform.up * jumpAccel);
+			grounded = false;
 		}
 
 		//Caps the player's horizontal velocity
