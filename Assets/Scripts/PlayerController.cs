f877f56dc7c20db9bd46054af9845f742b2fc6a8
BrianLam4101 <BrianLam4101@gmail.com>
2016-10-27 23:19:33 -0700

    Fixed Player Movement
    - Player movement scrit now decelerates player while they are on the
    ground
    - Player movement supports disabaling jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,20 @@ using System.Collections;
 [RequireComponent(typeof(Collider2D))]
 public class PlayerController : MonoBehaviour {
 
+    public string horizontalControl = "Horizontal";
+    public string jumpControl = "Jump";
+
 	private Rigidbody2D rb;
 	private bool grounded;
 	private Collider2D coll;
 
-	public float maxHorizontalSpeed = 5f;
+	public float maxHorizontalSpeed = 7f;
 	public float horizontalAccel = 50f;
-	public float jumpAccel = 500f;
+	public float jumpAccel = 300f;
+    public float deceleration = 0.5f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		rb = GetComponent<Rigidbody2D>();
 		coll = GetComponent<Collider2D>();
 	}
@@ -25,20 +29,16 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 	void FixedUpdate () {
-
-		if (Input.GetKey("right") || Input.GetKey (KeyCode.D)) {
-			rb.AddForce (transform.right * horizontalAccel);
-		}
-		if (Input.GetKey ("left") || Input.GetKey (KeyCode.A)) {
-			rb.AddForce (transform.right * -horizontalAccel);
-		}
-		if ((Input.GetKey ("up") || Input.GetKey (KeyCode.W)) && grounded) {
-			rb.AddForce (transform.up * jumpAccel);
-			grounded = false;
-		}
-
-		//Caps the player's horizontal velocity
-		Vector2 velocity = rb.velocity;
+        if (grounded) {
+            rb.velocity = new Vector3(rb.velocity.x - Mathf.Sign(rb.velocity.x) * Mathf.Min(Mathf.Abs(rb.velocity.x), deceleration) , rb.velocity.y);
+            if (Input.GetAxis(jumpControl) > 0) {
+                rb.AddForce(transform.up * jumpAccel);
+                grounded = false;
+            }
+        }
+        rb.AddForce(transform.right * Input.GetAxis(horizontalControl) * horizontalAccel * (grounded ? 1 : 0.25f));
+        //Caps the player's horizontal velocity
+        Vector2 velocity = rb.velocity;
 		velocity.x = Mathf.Clamp (velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed);
 		rb.velocity = velocity;
 	}
