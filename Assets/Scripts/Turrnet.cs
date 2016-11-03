da927ecaee389c30a4c14c4138e3f26027a835e6
BrianLam4101 <BrianLam4101@gmail.com>
2016-11-03 14:01:32 -0700

    Added turrent
    - new Arrow Prefab
    - Added turrent that fires arrows

---
+++ Assets/Scripts/Turrnet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turrnet : MonoBehaviour {
+
+    GameObject arrowPrefab;
+
+	// Use this for initialization
+	void Start () {
+        arrowPrefab = Resources.Load<GameObject>("Arrow");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Fire1") == 1) {
+            GameObject arrow = Instantiate<GameObject>(arrowPrefab);
+            arrow.transform.position = transform.position;
+            Vector3 direction = (new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, gameObject.transform.position.z) - transform.position).normalized;
+            float mouseDirection = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+            arrow.transform.rotation = Quaternion.Euler(0f, 0f, mouseDirection);
+            arrow.GetComponent<Rigidbody2D>().velocity = arrow.transform.up * 18;
+        }
+	}
+}
