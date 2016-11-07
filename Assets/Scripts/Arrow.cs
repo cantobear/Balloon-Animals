63f01ab50a3e613e60c5cd22741d1f4ae3d117c4
BrianLam4101 <BrianLam4101@gmail.com>
2016-11-07 14:48:41 -0800

    Updated Turrent
    - Arrows now dissapear when they touch the floor or ground
    - Now have to charge shot before fireing
    - The longer the arrow is charged the farther it will fly, releasing the
    fire button will shoot the arrow
    - Added a dotted line to indicate arrow trajectory

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 0.5f) {
+            Vector3 direction = gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
+            transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90);
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D c) {
+        if (c.collider.CompareTag("Wall"))
+            Destroy(gameObject);
+    }
+}
