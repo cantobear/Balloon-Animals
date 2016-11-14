9dcc131e587048853e4b1c9bcac8af744a416992
BrianLam4101 <BrianLam4101@gmail.com>
2016-11-14 11:53:48 -0800

    Arrows now despawn
    - Arrows will stick into walls, if the arrow is pointing perpendicular
    enough to the wall.  If not the arrow will just slide off, this is so
    the arrow doesnt teleport too far to get into the wall.
    - Arrows will despawn after 5 seconds of sticking ito a wall, this
    number can be changed.
    - Arrows slowly fade out when they despawn

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -3,21 +3,53 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 
+    public float despawnTime = 5f;
+
+    private Vector3 lastPosition;
+
 	// Use this for initialization
 	void Start () {
-	
+        lastPosition = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 0.5f) {
+	void FixedUpdate () {
+        if (GetComponent<Rigidbody2D>().velocity.magnitude > 0.1f) {
             Vector3 direction = gameObject.GetComponent<Rigidbody2D>().velocity.normalized;
             transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90);
         }
+        lastPosition = transform.position;
     }
 
     void OnCollisionEnter2D (Collision2D c) {
-        if (c.collider.CompareTag("Wall"))
-            Destroy(gameObject);
+        if (c.collider.CompareTag("Wall")) {
+            RaycastHit2D[] hit = Physics2D.LinecastAll(lastPosition - transform.up * GetComponent<BoxCollider2D>().bounds.extents.y, lastPosition + transform.up * 3f * GetComponent<BoxCollider2D>().bounds.extents.y);
+            bool didHit = false;
+            foreach (RaycastHit2D x in hit) {
+                if (x.collider.CompareTag(c.collider.tag)) {
+                    GetComponent<Rigidbody2D>().isKinematic = true;
+                    didHit = true;
+                    transform.position = lastPosition;
+                    transform.position += new Vector3(x.point.x, x.point.y) - transform.position;
+                    break;
+                }
+            }
+            if (didHit) {
+                GetComponent<Rigidbody2D>().isKinematic = true;
+                GetComponent<BoxCollider2D>().isTrigger = true;
+                StartCoroutine("deleteAfterDelay", despawnTime);
+            }
+        }
+    }
+
+    IEnumerator deleteAfterDelay(float delay) {
+        yield return new WaitForSeconds(delay);
+        Color c = Color.black;
+        c.a = GetComponent<SpriteRenderer>().color.a / 10;
+        do {
+            GetComponent<SpriteRenderer>().color -= c;
+            yield return new WaitForSeconds(0.1f);
+        } while (GetComponent<SpriteRenderer>().color.a > 0);
+        Destroy(gameObject);
     }
 }
