f7fdf64c129459261c49dc5416e4e9640ea07dd1
Brian Lam <lamb8@uci.edu>
2017-04-19 16:13:48 -0700

    Turrent has Arrow Count
    - Turrent now has a limited number of arrows, 5 (can be changed in
    inspector), and takes 1 second to rechage one (can also be changed in
    inspector)
    - Added basic temporary UI for arrow count
    - Turrnet charging reworked to start charging faster and exponentially
    slow down as it is charging to full
    - Charging now takes 1 second (can be changed in inspector)

---
+++ Assets/Scripts/ArrowCountUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowCountUI : MonoBehaviour {
+
+    public Turrnet turrent;
+    private Slider slide;
+
+	// Use this for initialization
+	void Start () {
+        slide = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slide.value = turrent.arrowCount;
+	}
+}
