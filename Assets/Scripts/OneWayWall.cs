8516c614b57927847d3b4ee8b2e91d574f6ba43b
Brian Lam <lamb8@uci.edu>
2017-04-28 03:43:25 -0700

    Added one way walls
    - One way walls will only block collisions in one direction

---
+++ Assets/Scripts/OneWayWall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayWall : MonoBehaviour {
+
+    private Collider col;
+
+	void Start () {
+        transform.GetChild(0).gameObject.AddComponent<TriggerDetection>();
+        col = GetComponent<Collider>();
+
+    }
+	
+	protected void enableCollision(Collider col) {
+        Physics.IgnoreCollision(this.col, col, false);
+        Debug.Log("enter");
+    }
+
+    protected void disableCollision(Collider col) {
+        Physics.IgnoreCollision(this.col, col);
+        Debug.Log("exit");
+    }
+
+    private class TriggerDetection : MonoBehaviour {
+        OneWayWall parent;
+
+        void Start() {
+            parent = transform.parent.GetComponent<OneWayWall>();
+        }
+
+        void OnTriggerEnter(Collider col) {
+            parent.disableCollision(col);
+        }
+
+        void OnTriggerExit(Collider col) {
+            parent.enableCollision(col);
+        }
+    }
+}
